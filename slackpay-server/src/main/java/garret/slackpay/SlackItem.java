23a7f826ffe01f1c8b570ebcbc57101bfd3629c8
garret.meier <garret.meier@bunchball.com>
2015-05-06 12:43:25 -0500

    adding the file

---
+++ slackpay-server/src/main/java/garret/slackpay/SlackItem.java
@@ -0,0 +1,15 @@
+package garret.slackpay;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+
+public class SlackItem {
+
+	public static String getSlackEmail (String username){
+		return username;
+	}
+	
+	public HttpResponse getSlackChannel(String team_id){
+		return null;
+	}
+}
