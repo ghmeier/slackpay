b84eae956ab2f6f2110faf04bfe8f734f37af653
garret.meier <garret.meier@bunchball.com>
2015-05-06 10:00:46 -0500

    added slack info

--- slackpay-server/src/main/java/garret/slackpay/PayResource.java
+++ slackpay-server/src/main/java/garret/slackpay/PayResource.java
@@ -2,6 +2,7 @@ package garret.slackpay;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -9,12 +10,13 @@ import javax.ws.rs.core.MediaType;
 
 @Path("pay")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class PayResource {
 	
-	@GET
-	public SlackpayResponse getPay(@QueryParam("to") String to, @QueryParam("from") String from, @QueryParam("amt") String amt){
+	@POST
+	public SlackInfo getPay(SlackInfo info){
 		
-		return new SlackpayResponse(to + " "+ from + " " + amt);
+		return info;
 		
 	}
 
